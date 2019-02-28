5931e1f19ce7ab544669a89c4666f6aef0a96fc2
Greg <greggordon98@gmail.com>
2019-02-28 14:38:16 -0800

    Updates

---
+++ precache-manifest.df01ac0d85facaf2f064aa9bdf79815d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81540dd2be0155dd82c4",
+    "url": "/course-directory-github/static/js/runtime~main.81540dd2.js"
+  },
+  {
+    "revision": "c27ce1f535e535c92f62",
+    "url": "/course-directory-github/static/js/main.c27ce1f5.chunk.js"
+  },
+  {
+    "revision": "f5ae2494a8dbd9f95f4b",
+    "url": "/course-directory-github/static/js/1.f5ae2494.chunk.js"
+  },
+  {
+    "revision": "c27ce1f535e535c92f62",
+    "url": "/course-directory-github/static/css/main.0398ae98.chunk.css"
+  },
+  {
+    "revision": "1b615db19e349ea719e22bdc46957a0a",
+    "url": "/course-directory-github/index.html"
+  }
+];
